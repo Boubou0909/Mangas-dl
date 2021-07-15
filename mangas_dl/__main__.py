c331d1abbb8cc3eceaf38d2af011eb114b33eff9
Balthazar Patiachvili <balthazar0909@gmail.com>
2021-07-16 00:37:29 +0200

    Correct error setup

--- mangas_dl/__main__.py
+++ mangas_dl/__main__.py
@@ -8,7 +8,7 @@ from .Headers.errors import ConnexionError
 from .Headers.functions_formatting import str_to_chapters
 from .Headers.functions_os import is_path_exists_or_creatable
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 ARGS = sys.argv[1:]
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 with open("readme.md", "r") as f:
     readme = f.read()
